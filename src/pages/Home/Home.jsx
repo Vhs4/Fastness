843353ce7b49758e05be417d4621a1546289a2bb
unknown <leticiazago100@gmail.com>
2024-03-09 12:56:36 -0300

    atualização da página

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './style.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+        <nav className="nav">
+           <img src="/img/logo.png" class="img_nav" alt="logo fastness"></img>
+        <ul>
+            <li><a href="">Benefícios</a></li>
+            <li><a href="">Sobre a fastness</a></li>
+            <li className=""><a href="">Central de ajuda</a></li>
+            <li className=""><a href="">Entrar</a></li>
+            <button className="header_button">Cadastre-se</button>
+        </ul>
+        </nav>
   )
 }
 
