7510a113bc00989ff339a1360fe876d84a2f2930
unknown <leticiazago100@gmail.com>
2024-03-19 22:18:42 -0300

    mobile landing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,81 @@
 import React from 'react'
 import './style.css'
 
+
 const Home = () => {
+  const [navMenu, setNavMenu] = React.useState(false)
+  function menuShow() {
+    setNavMenu(!navMenu)
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu-aberto (3).png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/menuabertoo.png";
+    }
+  }
   return (
         <>
-        <nav class="navP">
-          <img src="/img/logo.png" class="img_nav" alt="logo fastness"></img>
+        <div className="containerGeral">
+        <nav className="navP">
+          <img src="/img/logo.png" className="img_nav" alt="logo fastness"></img>
         <ul>
             <li><a href="">Sobre</a></li>
             <li><a href="">Benefícios</a></li>
             <li className=""><a href="">Entrar</a></li>
-            <button className="header_button">Cadastre-se</button>
         </ul>
+        <button className="header_button">Cadastre-se</button>
+
+        <div className="mobile-menu-icon">
+          <button onClick={menuShow}><img className="icon" src="img/menu-aberto (3).png" alt="menu-white"></img></button>
+        </div>
+
         </nav>
+
+        <div className="mobile-menu"> 
+{navMenu && (
+<>
+        <ul>
+            <li><a href="#" className="nav-item">Sobre</a></li>
+            <li><a href="#" className="nav-item">Benefícios</a></li>
+            <li><a href="" className="nav-item">Entrar</a></li>
+        </ul>
+        <button className="header_button">Cadastre-se</button>
+</>
+)}
+</div>
         <p className="p1">
-        Crie laços <span>rápidos</span> e <br></br>  
-        feche <span>negócios</span> em <br></br>
+        Crie laços <span className="span1">rápidos</span> e <br></br>  
+        feche <span className="span1">negócios</span> em <br></br>
         um piscar de olhos.
         </p>
+
+        <p className="p1-mobile">
+        Crie laços <span className="mobilep1">rápidos</span> e <br></br>  
+        feche <span className="mobilep1">negócios</span> em <br></br>
+        um piscar de olhos.
+        </p>
+
+
         <p className="p2">
-        Com a <span>Fastness</span>, os laços comerciais <br></br>
-        são tecidos em ambientes virtuais para <br></br> conversas, impulsionando os <span>negócios</span> <br></br>
+        Com a <span className="span1">Fastness</span>, os laços comerciais <br></br>
+        são tecidos em ambientes virtuais para <br></br> conversas, impulsionando os <span className="span1">negócios</span > <br></br>
         em um piscar de olhos!
         </p>
-        <button className="button1">Comece gratuitamente</button>
-        <img src="/img/home.png" class="img_header" alt="homem de negócios"></img>
-        <img src="/img/celular.png" class="img_header1" alt="celular"></img>
+
+
+        <a className="button1">Comece gratuitamente</a>
+        <img src="/img/home.png" className="img_header" alt="homem de negócios"></img>
+        <img src="/img/celular.png" className="img_header1" alt="celular"></img>
         <p className="p3">Sobre a Fastness</p>
         <p className="p4">Oferecemos o melhor ambiente para aumentar seus <br></br>laços comerciais e fechar  
         negócios de maneira rápida<br></br> e prática. Com nosso site, fica muito mais rápido<br></br> encontrar 
         representantes de acordo com seus <br></br>interesses, além de ter a opção de bate-papo via chat.  <br></br>Na 
         conclusão da sua negociação, é possível escolher <br></br> um lugar para se encontrar e conversar pessoalmente <br></br> 
         com o representante do seu interesse.</p>
-        <img src="/img/home2.png" class="img_header2" alt="homem de negócios"></img>
+        
+        <img src="/img/home2.png" className="img_header2" alt="homem de negócios"></img>
 
         <section className="section1">
         <div className="div1">
@@ -44,7 +87,7 @@ const Home = () => {
           <p className="p4section1">Através do chat, <br></br>você pode <br></br>conversar <br></br>comercialmente e <br></br>marcar encontros!</p>
         </div>
         </section>
-        <img src="/img/imagemraio.png" class="img_section" alt="raio"></img>
+        <img src="/img/imagemraio.png" className="img_section" alt="raio"></img>
         <section className="section2">
           <div className="div3">
             <p className="p1section2">Fast business</p>
@@ -58,6 +101,7 @@ const Home = () => {
         <footer>
           <p className="footer">© 2024 - Fastness, Todos os direitos reservados</p>
         </footer>
+        </div>
         </>
   )
 }
--- src/pages/PaginaInicial/PaginaInicial.jsx
+++ src/pages/PaginaInicial/PaginaInicial.jsx
@@ -1,17 +1,18 @@
 
+import Carousel from 'react-bootstrap/Carousel';
 import './paginainicial.css';
 
 export const PaginaInicial = () => {
     return (
         <>
             <nav className="navP">
-                <img src="/img/logo.png" class="img_nav" alt="logo fastness"></img>
+                <img src="/img/logo.png" className="img_nav" alt="logo fastness"></img>
                 <ul>
                     <li><a href="">Início</a></li>
                     <li><a href="">Negócios</a></li>
                     <li className=""><a href="">Locais</a></li>
                     <li className=""><a href="">Chat</a></li>
-                    <img src="/img/iconeboneco23.png" class="img_nav2" alt="ícone boneco"></img>
+                    <img src="/img/iconeboneco23.png" className="img_nav2" alt="ícone boneco"></img>
                 </ul>
             </nav>
             <p className="p1">
@@ -21,6 +22,35 @@ export const PaginaInicial = () => {
                 Com a <span>Fastness</span>, os negócios fecham em um piscar de <br></br>olhos! Explore o melhor de nossas <span>funcionalidades</span>!
             </p>
             <img src="/img/imgnegocios2.png" className="img-header" alt="fechando negócios"></img>
+
+            <section className="div-section">
+            <div className="info1">
+               <h2 className="div1-h2">Negócios</h2>
+               <p>- Interesses individuais;</p>
+               <p>- Negócios nichados;</p>
+               <p>- Grandes empresas;</p>
+               <p>- Credibilidade.</p>
+               <button className="div-button">Acessar</button>
+            </div>
+            <div className="info2">
+               <h2 className="div2-h2">Locais</h2>
+               <p>- Locais especializados;</p>
+               <p>- Encontros comerciais;</p>
+               <p>- Conversas eficientes;</p>
+               <p>- Olho no olho.</p>
+               <button className="div-button">Acessar</button>
+            </div>
+            <div className="info3">
+               <h2 className="div3-h2">Chat</h2>
+               <p>- Fácil comunicação;</p>
+               <p>- Dúvidas esclarecidas;</p>
+               <p>- Interesses bem definidos;</p>
+               <p>- Laços criados.</p>
+               <button className="div-button">Acessar</button>
+            </div>
+            </section>
+
+
         </>
-        )
+    )
 }
